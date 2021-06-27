55f578853145a9c254b301419d35e7a51fc89e08
graphdr <graphdoctor@gmail.com>
2021-06-27 00:17:46 -0400

    ch work

---
+++ slides/superceded/DefineColors.R
@@ -0,0 +1,54 @@
+#-----------------------------------------------------------
+#
+#   DefineColors
+#
+#   2013-08-18. rev. 2015-06-02
+#   RAL
+#
+#-----------------------------------------------------------
+
+library(RColorBrewer)
+
+# brown/blue-green (divergent)
+BrBG <- brewer.pal(10, "BrBG")
+darkBr  <- BrBG[1]
+medBr   <- BrBG[2]
+lightBr <- BrBG[3]
+paleBr  <- BrBG[4]
+
+minBr   <- BrBG[5]
+minBG   <- BrBG[6]
+
+paleBG  <- BrBG[7]
+lightBG <- BrBG[8]
+medBG   <- BrBG[9] 
+darkBG  <- BrBG[10]
+
+# purple/green (divergent)
+PrGn <- brewer.pal(10, "PRGn")
+darkPr  <- PrGn[1]
+medPr   <- PrGn[2]
+lightPr <- PrGn[3]
+palePr  <- PrGn[4]
+
+minPr   <- PrGn[5]
+minGn   <- PrGn[6]
+
+paleGn  <- PrGn[7]
+lightGn <- PrGn[8]
+medGn   <- PrGn[9] 
+darkGn  <- PrGn[10]
+
+# grays
+Grays <- brewer.pal(9,"Greys")
+gray1 <- Grays[1]
+gray2 <- Grays[2]
+gray3 <- Grays[3]
+gray4 <- Grays[4]
+gray5 <- Grays[5] 
+gray6 <- Grays[6]
+gray7 <- Grays[7]
+gray8 <- Grays[8]
+gray9 <- Grays[9] 
+
+# last line      
